b53e113fae8d1dc9a30fab8509c5529003cd7ea0
KorNatalya <natalie178@ya.ru>
2018-07-12 20:16:27 +0300

    make nav

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('div.acco2__trigger').click(function() {
+    if (!$(this).hasClass('active')) {	//если "кликнутый" пункт неактивный:
+      $('div.acco2__trigger').removeClass('active').next('ul').slideUp();
+      //делаем неактивными все пункты и скрываем все блоки
+      $(this).addClass('active');	//активируем "кликнутый" пункт
+      $(this).next('ul').slideDown(200);	//раскрываем следующий за ним блок с описанием
+    } else {	//иначе:
+      $(this).removeClass('active').next('ul').slideUp();	//скрываем данный пункт
+    }
+  });
+});
